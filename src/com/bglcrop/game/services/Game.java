b102a033bca8ad79924104188635cec4ec4a9e7d
SachinthaHG <sachintha.45568@gmail.com>
2018-04-05 00:43:46 +1000

    CGL: code formatting and commenting

--- src/com/bglcrop/game/services/Game.java
+++ src/com/bglcrop/game/services/Game.java
@@ -33,35 +33,42 @@ public class Game {
         gridOfSquareCells = new int[NUMBER_OF_ROWS][NUMBER_OF_COLS];
     }
 
+    /**
+     * Starting point of the game
+     */
     public void start() {
         processInput();
         processGridOfCells();
     }
 
-
     private void processInput() {
+        /*Mark live cells on the grid*/
         inputLiveCells.forEach(cell -> gridOfSquareCells[cell.getX()][cell.getY()] = 1);
     }
 
     private void processGridOfCells() {
         int[][] tempGridOfSquareCells = new int[NUMBER_OF_ROWS][NUMBER_OF_COLS];
 
-        GameUtilities.gridCopy(tempGridOfSquareCells, gridOfSquareCells);
+        GameUtilities.gridCopy(tempGridOfSquareCells, gridOfSquareCells); //Copy the grid into a temporary grid
 
         for (int k = 0; k < NUMBER_OF_STAGES; k++) {
             List<Point> currentStageLiveCells = new ArrayList<>();
 
+            /*Iterating through each row in the grid*/
             for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+                /*Iterating through each column*/
                 for (int j = 0; j < NUMBER_OF_COLS; j++) {
                     List<Point> neighbours = GameUtilities.findNeighbours(i, j, NUMBER_OF_COLS, NUMBER_OF_ROWS);
 
                     int liveNeighbourCount = 0;
+                    /*Find the number of live neighbour cells*/
                     for (Point neighbour : neighbours) {
                         if (gridOfSquareCells[neighbour.getX()][neighbour.getY()] == 1) {
                             liveNeighbourCount++;
                         }
                     }
 
+                    /*Update the temporary grid cell states based on the given rules*/
                     if (liveNeighbourCount < 2 && gridOfSquareCells[i][j] == 1) {
                         tempGridOfSquareCells[i][j] = 0;
                     } else if (liveNeighbourCount <= 3 && liveNeighbourCount >= 2 && gridOfSquareCells[i][j] == 1) {
@@ -79,12 +86,14 @@ public class Game {
             }
 
             processOutput(currentStageLiveCells);
+
+            /*Copy temporary grid into the original grid*/
             GameUtilities.gridCopy(gridOfSquareCells, tempGridOfSquareCells);
         }
     }
 
     private void processOutput(List<Point> liveCellList) {
+        /*Format and print the output*/
         GameUtilities.printCellList(liveCellList);
     }
-
 }
